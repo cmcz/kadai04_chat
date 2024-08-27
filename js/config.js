ecaa6223a796160cd46c764122453224d511ff4f
CCM <choco@CCMs-MacBook-Air.local>
2024-08-27 22:08:35 +0900

    fix: github secret

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "${{ secrets.FIREBASE_API_KEY }}",
+    authDomain: "ada-test-82ce6.firebaseapp.com",
+    projectId: "ada-test-82ce6",
+    storageBucket: "ada-test-82ce6.appspot.com",
+    messagingSenderId: "115730988268",
+    appId: "1:115730988268:web:d255e2aef4c6fb6f8339c9"
+};
